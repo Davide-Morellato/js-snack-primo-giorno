683af0438cf049a6d7dfa5277ee635c6314e8383
Davide-Morellato <morellato.davide@yahoo.it>
2024-03-01 14:38:34 +0100

    Exercises

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Dichiara una variabile chiamata number e assegnale il valore 100.
+// Stampa il valore della variabile in console.
+
+let number = 100 ;  //number
+
+console.log(number);  //number
+
+
+
+// Dichiara una costante chiamata PI e assegnale il valore di 3.14.
+// Stampa il valore della costante in console.
+
+const PI = 3.14 ; //number
+
+console.log(PI) ; //number
+
+
+
+// Prova ad assegnare alla costante chiamata PI il valore di 3.1416.
+// Sai dire cosa succede?
+
+const PI = 3.1416 ; //number
+
+console.log(PI) ; // ERRORE -> perchè la const PI è stata dichiarata già nell'esercizio precedente (const è una Variabile NON Riassegnabile)
+
+
+
+// Dichiara una variabile chiamata radius e assegnale il valore 8.
+// Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+// Stampa il valore della variabile circle in console.
+
+const radius = 8 ; //number
+const circle = (radius * 2 * PI); //number
+
+console.log(circle) //number
+
+
+
+// Dichiara una variabile chiamata name e assegnale il valore “Carlo”.
+// Successivamente, modifica il valore della variabile in “Marco”.
+// Stampa il nuovo valore in console.
+
+let nome = "Carlo" ;  //string
+nome = "Marco" ; //string
+
+console.log(nome) ; //string
